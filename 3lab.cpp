1e943ab63a907384657aff553fb3398ee3e8dcb5
DealerDEAL <112918405+DealerDEAL@users.noreply.github.com>
2022-11-13 22:31:37 +0700

    Add files via upload

--- 3lab.cpp
+++ 3lab.cpp
@@ -11,28 +11,32 @@ int main()
      int  OriginalNumber = 0;
 	 int  EnteredNumber = 0;
 	 int  CountNumber =0;
+	 char  newline;
      fscanf(num,"%d",&OriginalNumber);
      bool completed = false;
      fprintf(output,"entered number/n");
-     do()
-     {
-         cin >> EnteredNumber;
-         if (EnteredNumber > OriginalNumber) {
+     do{
+     	cin >> EnteredNumber;
+     	if (scanf("%d%c" &EnteredNumber, &newline) EnteredNumber>=1 || newline != '\n' || ){
+         	if (EnteredNumber > OriginalNumber) {
              fprintf(output,&EnteredNumber"/n");
 			 cout <<"enter a lower number";
 			 CountNumber++;
-         } 
-		 else if EnteredNumber < OriginalNumber) {
+         	} 
+			 else if EnteredNumber < OriginalNumber) {
 			fprintf(output,&EnteredNumber"/n");
 			cout <<"enter a larger number";
 			CountNumber++;
-		}
-		 else if (EnteredNumber == OriginalNumber){
+			}
+		 	else if (EnteredNumber == OriginalNumber){
 			fprintf(output,&EnteredNumber"/n");
 			cout <<"comleted";
 			CountNumber++;
 			completed = true;
+			}
 		}
+		else
+		cout <<"eror, plese try again";
      }while(completed != true);
      fprintf(output,"total numbers entered:",&CountNumber);
      return 0;
