c61d05e6440b9be28105e58d1ca7f45a11b4ad27
DealerDEAL <112918405+DealerDEAL@users.noreply.github.com>
2022-12-06 14:52:38 +0700

    Update 3lab.cpp
    G

--- 3lab.cpp
+++ 3lab.cpp
@@ -6,37 +6,45 @@ using namespace std;
 int main()
 {
      FILE *num , *output;
-     num = fopen("startgame.txt", "r");
      output = fopen("endgame.txt","w");
+     fprintf(output,"entered number/n");
+     fclose(output);
      int  OriginalNumber = 0;
 	 int  EnteredNumber = 0;
 	 int  Count_Number =0;
 	 char  newline;
      bool completed = false;
-     fprintf(output,"entered number/n");
      while(completed == false) {
          cout<<"Entere Number";
      	 cin >> EnteredNumber;
+     	 num = fopen("startgame.txt", "r");
      	 fscanf(num,"%d",&OriginalNumber);
          	if (EnteredNumber > OriginalNumber) {
+         	    output = fopen("endgame.txt","w");
                 fprintf(output,"%d",EnteredNumber);
+                fclose(output);
 			    cout <<"enter a lower number";
 			    Count_Number++;
          	} 
 			if (EnteredNumber < OriginalNumber) {
+			    output = fopen("endgame.txt","w");
 			    fprintf(output,"%d",EnteredNumber);
+			    fclose(output);
 		        cout <<"enter a larger number";
 			    Count_Number++;
 			}
 			if (EnteredNumber == OriginalNumber){
+			    output = fopen("endgame.txt","w");
 			    fprintf(output,"%d",EnteredNumber);
+			    fclose(output);
 			    cout <<"comleted";
 			    Count_Number++;
 			    completed = true;
 			}
+		 fclose(num);
 		}
+	 output = fopen("endgame.txt","w");
      fprintf(output,"total numbers entered: %d", Count_Number);
-     fclose(num);
      fclose(output);
      return 0;
 }
